76d7ae703f4d29ee75681d90cbc154977f29f581
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-01-24 16:24:41 +0900

    ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '1111'
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :last_name_kanji, :last_name_kana, :first_name_kanji, :first_name_kana, :birthday])
+  end
+
 end
