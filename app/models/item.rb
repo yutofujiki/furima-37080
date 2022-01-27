3a0a8b4847dcf5d605c8b108920f77df5995da48
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-01-27 16:35:54 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,15 @@ class Item < ApplicationRecord
   belongs_to :user
   #has_one : buyer
 
-  validates :name,          presence: true
-
+  validates :name,                 presence: true
+  validates :price,                presence: true
+  validates :explanation,          presence: true
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id,          numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_price_id,    numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_day_id,      numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :situation_id,         numericality: { other_than: 1 , message: "can't be blank"} 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :category
