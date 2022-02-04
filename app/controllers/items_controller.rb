8e052bcb3dd8bb7a7ef7489c1d8501cb8a7b9786
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-04 15:52:52 +0900

    商品詳細表示機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  
   root to: "items#index"
 
-  #resources :users, only: [:new, :create] do
-    resources :items, only: [:index, :create, :new]
-  #end
-
+    resources :items
 
 end
