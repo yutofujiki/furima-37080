535a2021a74e97e66050116a6adc5db9821fe625
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-05 20:02:29 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_edit, only: :edit
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update,:move_to_edit]
 
   def index
     @items =Item.all.order("created_at DESC")
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_edit
-    @item = Item.find(params[:id])
       unless @item.user.id == current_user.id
         redirect_to root_path
       end
