03de67b4126087efcaadfdd33a6eea4e4db8a4f9
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-05 22:29:53 +0900

    before actionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update ]
   before_action :move_to_edit, only: :edit
-  before_action :set_item, only: [:show, :edit, :update,:move_to_edit]
 
   def index
     @items =Item.all.order("created_at DESC")
@@ -41,14 +41,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :explanation, :image, :price, :category_id,:delivery_price_id, :situation_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 
-  def move_to_edit
-      unless @item.user.id == current_user.id
-        redirect_to root_path
-      end
-  end
-
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_edit
+    unless @item.user.id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
