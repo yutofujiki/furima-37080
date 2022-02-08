2d3091829fa9a46334eb0173b51a58b2f9f47693
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-08 16:13:08 +0900

    購入機能に必要なモデルやコントローラーを作成する

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,22 @@
 class BuyersController < ApplicationController
 
   def index
+    @buyer_address = BuyerAddress.new
   end
+
+  def create
+    @buyer_address =BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def buyer_params
+    params.require(buyer_address).permit(:yubin_number, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-    resources :items
-    resources :buyers, only: [:index, :create]
-
+    resources :items do
+     resources :buyers, only: [:index, :create]
+    end
+    
 end
