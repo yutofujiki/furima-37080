95c8719322f93c6f7e074fee5b31747ad06f2c9c
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-10 20:11:33 +0900

    エラーメッセージまで

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -6,7 +6,7 @@ class BuyersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @item = Item.find(params[:item_id])
     @buyer_address =BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
       @buyer_address.save
@@ -18,12 +18,7 @@ class BuyersController < ApplicationController
 
   private
   def buyer_params
-    params.require(buyer_address).permit(:yubin_number, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id)
+    params.require(:buyer_address).permit(:yubin_number, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-
-  def item_params
-    params.require(:item).permit(:name, :explanation, :image, :price, :category_id,:delivery_price_id, :situation_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
-  end
-
 
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -6,14 +6,14 @@ class BuyerAddress
     validates :yubin_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :address
-    validates :phone_number, format: {with: /\A\d{11}\z/, message: "11桁の半角英数字で入力してください"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "11桁の半角英数字で入力してください"}
   end
     validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank"} 
 
     def save
       buyer = Buyer.create(user_id: user_id, item_id: item_id)
 
-      Address.create(yubin_number: yubin_number, prefecture_id: prefecture.id, municipality: municipality, address: address, phone_number: phone_number, buyer_id: buyer.id)
+      Address.create(yubin_number: yubin_number, prefecture_id: prefecture_id, municipality: municipality, address: address, phone_number: phone_number, buyer_id: buyer.id, building: building)
     end
 
 end
