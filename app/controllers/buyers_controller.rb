d908ec608b7416627f0f4f29e391e7ffbab90e56
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-10 15:09:11 +0900

    商品購入ページでitemの名前などを取得

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,10 +1,12 @@
 class BuyersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new
   end
 
   def create
+    binding.pry
     @buyer_address =BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
       @buyer_address.save
@@ -18,5 +20,10 @@ class BuyersController < ApplicationController
   def buyer_params
     params.require(buyer_address).permit(:yubin_number, :prefecture_id, :municipality, :address, :building, :phone_number).merge(user_id: current_user.id)
   end
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :image, :price, :category_id,:delivery_price_id, :situation_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+
 
 end
