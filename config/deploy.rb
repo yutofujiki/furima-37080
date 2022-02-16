00c34ef6a51987834607321222fe2526d9f1e109
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-16 16:36:36 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-37080'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yutofujiki/Recent Repositories.git'
+set :repo_url,  'git@github.com:yutofujiki/furima-37080.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
