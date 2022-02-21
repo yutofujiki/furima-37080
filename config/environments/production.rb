e0b693c83581f31adeaa376a4814393d2396279b
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-02-21 15:25:20 +0900

    herokuデブロイ設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://【Herokuアプリ名】.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://furima37080.herokuapp.com', 'http://furima-37080.herokuapp.com']
 end
